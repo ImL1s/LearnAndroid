5b855cf46cfcadfef0e60b6d1f5e8a350eab54c5
ImL1s <1032448217@gm.cute.edu.tw>
2016-10-10 16:21:39 +0800

    update

---
+++ LearnAndroid/04DataStorage/03sqlite/src/main/java/com/learn/aa223/a03sqlite/MainActivity.java
@@ -0,0 +1,13 @@
+package com.learn.aa223.a03sqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
