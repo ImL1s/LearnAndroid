3a31f5350aef15b464e0bb1982826f0628a20a30
ImL1s <ImL1s@outlook.com>
2017-02-09 14:59:18 +0800

    blackList OK

---
+++ ActualCombatProject/Android-ActualCombat-01SafeBodyguard/SafeBodyguard/app/src/main/java/com/demo/safeBodyGuard/engine/SmsEngine.java
@@ -0,0 +1,15 @@
+package com.demo.safeBodyGuard.engine;
+
+/**
+ * Created by ImL1s on 2017/2/9.
+ * <p>
+ * DESC:
+ */
+
+public class SmsEngine
+{
+    public void Backup()
+    {
+
+    }
+}
--- ActualCombatProject/Android-ActualCombat-01SafeBodyguard/SafeBodyguard/app/src/main/java/com/demo/safeBodyGuard/service/BlackListService.java
+++ ActualCombatProject/Android-ActualCombat-01SafeBodyguard/SafeBodyguard/app/src/main/java/com/demo/safeBodyGuard/service/BlackListService.java
@@ -2,30 +2,34 @@ package com.demo.safeBodyGuard.service;
 
 import android.app.Service;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.ContentObserver;
 import android.net.Uri;
+import android.os.Handler;
 import android.os.IBinder;
-import android.os.RemoteException;
-import android.provider.Telephony;
+import android.os.Message;
 import android.telephony.PhoneStateListener;
 import android.telephony.SmsMessage;
 import android.telephony.TelephonyManager;
 
 import com.android.internal.telephony.ITelephony;
-import com.demo.safeBodyGuard.db.BlackListOpenHelper;
 import com.demo.safeBodyGuard.db.dao.BlackListDAO;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class BlackListService extends Service
 {
+    private final static String CONTENT_URI_CALL_LOG = "content://call_log/calls";
+
+
     private BlackListDAO       mBlackListDAO       = null;
     private InnerSMSReceiver   mSmsReceiver        = null;
     private TelephonyManager   mTelephonyManager   = null;
     private PhoneStateListener mPhoneStateListener = null;
+    private InnerHandler       mHandler            = new InnerHandler();
 
     public BlackListService()
     {
@@ -44,22 +48,21 @@ public class BlackListService extends Service
         registerReceiver(mSmsReceiver, intentFilter);
 
         mTelephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        mTelephonyManager.listen(mPhoneStateListener = new BlackListPhoneStateListener(),
+        mTelephonyManager.listen(mPhoneStateListener = new InnerBlackListPhoneStateListener(),
                                  PhoneStateListener.LISTEN_CALL_STATE);
     }
 
     @Override
     public IBinder onBind(Intent intent)
     {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 
     @Override
     public void onDestroy()
     {
-        unregisterReceiver(mSmsReceiver);
-        mTelephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_NONE);
+        if(mSmsReceiver != null) unregisterReceiver(mSmsReceiver);
+        if(mPhoneStateListener != null && mTelephonyManager != null) mTelephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_NONE);
     }
 
     class InnerSMSReceiver extends BroadcastReceiver
@@ -91,12 +94,15 @@ public class BlackListService extends Service
     }
 
 
-    class BlackListPhoneStateListener extends PhoneStateListener
+    class InnerBlackListPhoneStateListener extends PhoneStateListener
     {
+
+        private ContentObserver mObserver;
+
         @Override
         public void onCallStateChanged(int state, String incomingNumber)
         {
-            if (state == TelephonyManager.CALL_STATE_RINGING)
+            if (state == TelephonyManager.CALL_STATE_RINGING && needBlock(incomingNumber))
             {
                 try
                 {
@@ -106,30 +112,66 @@ public class BlackListService extends Service
                     ITelephony telephony = ITelephony.Stub.asInterface(binder);
                     telephony.endCall();
 
-                    getContentResolver().delete(Uri.parse("content://call_log/calls"),
-                                                 "number = ?",
-                                                new String[]{incomingNumber});
+                    if (mObserver != null)
+                        getContentResolver().unregisterContentObserver(mObserver);
+
+                    mObserver = new InnerCallLogObserver(mHandler, incomingNumber,
+                                                         getContentResolver());
+                    getContentResolver()
+                            .registerContentObserver(Uri.parse(CONTENT_URI_CALL_LOG), false, mObserver);
                 }
-                catch (ClassNotFoundException e)
+                catch (Exception e)
                 {
                     e.printStackTrace();
                 }
-                catch (NoSuchMethodException e)
-                {
-                    e.printStackTrace();
-                }
-                catch (InvocationTargetException e)
-                {
-                    e.printStackTrace();
-                }
-                catch (IllegalAccessException e)
-                {
-                    e.printStackTrace();
-                }
-                catch (RemoteException e)
-                {
-                    e.printStackTrace();
-                }
+            }
+        }
+
+        /**
+         * 判斷是否為黑名單裡的電話號碼
+         * @param incomingNumber
+         * @return
+         */
+        private boolean needBlock(String incomingNumber)
+        {
+            int mode = BlackListDAO.getInstance(getApplicationContext()).getMode(incomingNumber);
+            return mode == 0 || mode == 2;
+        }
+    }
+
+
+    static class InnerHandler extends Handler
+    {
+        @Override
+        public void handleMessage(Message msg)
+        {
+
+        }
+    }
+
+
+    static class InnerCallLogObserver extends ContentObserver
+    {
+        private String          mIncomingNumber = null;
+        private ContentResolver mResolver       = null;
+
+
+        InnerCallLogObserver(Handler handler, String incomingNumber, ContentResolver resolver)
+        {
+            super(handler);
+            this.mIncomingNumber = incomingNumber;
+            this.mResolver = resolver;
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri)
+        {
+            super.onChange(selfChange, uri);
+
+            if (uri.toString().equals(CONTENT_URI_CALL_LOG))
+            {
+                mResolver.delete(Uri.parse(CONTENT_URI_CALL_LOG), "number = ?",
+                                 new String[]{mIncomingNumber});
             }
         }
     }
