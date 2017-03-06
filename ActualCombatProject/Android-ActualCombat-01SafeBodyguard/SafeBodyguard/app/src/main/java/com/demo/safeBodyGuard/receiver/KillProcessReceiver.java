3e303e8f437b98bec89c5a4b89f2c222e9633438
ImL1s <ImL1s@outlook.com>
2017-03-06 14:18:24 +0800

    feat: App widget

---
+++ ActualCombatProject/Android-ActualCombat-01SafeBodyguard/SafeBodyguard/app/src/main/java/com/demo/safeBodyGuard/receiver/KillProcessReceiver.java
@@ -0,0 +1,13 @@
+package com.demo.safeBodyGuard.receiver;
+
+import android.content.BroadcastReceiver;
+
+
+/**
+ * Created by ImL1s on 2017/3/6.
+ * <p>
+ * DESC:
+ */
+
+public class KillProcessReceiver extends BroadcastReceiver
+{}
